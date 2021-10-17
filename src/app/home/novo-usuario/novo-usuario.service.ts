2b2e004285e49608fb4f5a619e4612b68485d953
Aldrey <aldreybp@gmail.com>
2021-10-17 16:17:38 -0300

    Serviço de Novo Usuário

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  cadastraNovoUsuario(NovoUsuario:NovoUsuario){
+    return this.http.post('http://localhost:3000/user/signup', NovoUsuario); 
+  }
+}
