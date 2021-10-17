8e718a2d5582a83e5e59b755cb33da0fce0a9a9d
Aldrey <aldreybp@gmail.com>
2021-10-17 11:51:56 -0300

    Validação de campos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MensagemModule } from './componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MensagemModule
   ],
   providers: [],
   bootstrap: [AppComponent]
