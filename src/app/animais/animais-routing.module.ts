239b356e9bb1ac876a3162561eb51d9646aae567
Aldrey <aldreybp@gmail.com>
2021-10-20 15:42:30 -0300

    Guarda Resolver

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,12 +1,17 @@
+
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
+    resolve:{
+      animais:ListaAnimaisResolver,
+    }
   },
   {
     path:':animalId',
