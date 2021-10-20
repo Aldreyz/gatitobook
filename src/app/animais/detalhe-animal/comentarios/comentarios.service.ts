6404f6f0702b021a5e4eac464d43c413abbac810
Aldrey <aldreybp@gmail.com>
2021-10-20 19:48:28 -0300

    correção de API

--- src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
@@ -1,7 +1,7 @@
 import { Comentarios } from './comentarios';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const API = environment.apiURL;
@@ -10,13 +10,13 @@ const API = environment.apiURL;
   providedIn: 'root',
 })
 export class ComentariosService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,) {}
 
   buscaComentario(id: number): Observable<Comentarios> {
     return this.http.get<Comentarios>(`${API}/photos/${id}/comments`);
   }
-  incluiComentario(id: number, commentText: string): Observable<Comentario> {
-    return this.http.post<Comentario>(`${API}/photos/${id}/comments`, {
+  incluiComentario(id: number, commentText: string): Observable<Comentarios> {
+    return this.http.post<Comentarios>(`${API}/photos/${id}/comments`, {
       commentText,
     });
   }
